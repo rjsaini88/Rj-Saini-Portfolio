8257e9dea65db00219a75e056e75cabe41e1b1e9
Rj Saini <rsaini88@gmail.com>
2023-03-05 17:55:14 -0800

    Created and exported porjects to portfolio

--- src/componenets/Projects.jsx
+++ src/componenets/Projects.jsx
@@ -1,15 +1,50 @@
 // TODO create data for all the projects in a separate component
 //create a cards wrapper that wraps all the projects in a card wrapper
+// card to wrap the section
+import { projects } from "./DataInfo";
 
 export function Projects() {
   return (
-    <section id="projects">
+    <section id="projects" className="text-yellow-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <h1>App I've Built</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            These are some of the app I've built during my course at Bootcamp.
+          </p>
+        </div>
 
-    <div className="w-screen h-screen flex items-center justify-center">
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-100 p-4"
+            >
+              <div className="flex relative">
+                <img
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src="{project.imnage"
+                  alt="Project Image"
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-pink mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+
+      {/* <div className="w-screen h-screen flex items-center justify-center">
       <span>Hello World</span>
-    </div>
-
-
+    </div> */}
     </section>
-  )
+  );
 }
--- src/componenets/Skills.jsx
+++ src/componenets/Skills.jsx
@@ -1,7 +1,13 @@
 export default function Skills() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <span>Hello World</span>
-    </div>
+//use style card here. create and import card
+    <section id="skills">
+
+
+
+    </section>
+    // <div className="w-screen h-screen flex items-center justify-center">
+    //   <span>Hello World</span>
+    // </div>
   )
 }
